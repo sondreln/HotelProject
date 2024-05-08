c76f25ac6ec2b9534711e5db0afff125b968afdb
Sondre <sondrelangedal.ness@gmail.com>
2024-05-08 09:05:24 +0200

    Mappestrukt

--- HotelProject.Client/Data/HotelTask.cs
+++ HotelProject.Client/Data/HotelTask.cs
@@ -2,6 +2,7 @@ namespace HotelProject.Data
 {
     public class HotelTask
 {
+    public int Id { get; set; }
     public required string Description { get; set; }
     public required string Status { get; set; }
 }
